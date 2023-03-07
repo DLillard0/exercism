a4cd467ac044e7ddd67e353a76acf3c1deb1f346
袁俊凯 <yuanjunkai@linklogis.com>
2023-03-07 15:43:07 +0800

    finish: rna transcription

---
+++ rust/rna-transcription/src/lib.rs
@@ -0,0 +1,47 @@
+#[derive(Debug, PartialEq, Eq)]
+pub struct Dna(String);
+
+#[derive(Debug, PartialEq, Eq)]
+pub struct Rna(String);
+
+impl Dna {
+    pub fn new(dna: &str) -> Result<Dna, usize> {
+        match dna.chars().enumerate().find(|(_, c)| {
+            match c {
+                'A' | 'C' | 'G' | 'T' => false,
+                _ => true
+            }
+        }) {
+            Some((i, _)) => Err(i),
+            None => Ok(Dna(dna.to_string()))
+        }
+    }
+
+    pub fn into_rna(self) -> Rna {
+        Rna(
+            self.0.chars().map(|c| {
+                match c {
+                    'G' => 'C',
+                    'C' => 'G',
+                    'T' => 'A',
+                    'A' => 'U',
+                    _ => panic!()
+                }
+            }).collect()
+        )
+    }
+}
+
+impl Rna {
+    pub fn new(rna: &str) -> Result<Rna, usize> {
+        match rna.chars().enumerate().find(|(_, c)| {
+            match c {
+                'C' | 'G' | 'A' | 'U' => false,
+                _ => true
+            }
+        }) {
+            Some((i, _)) => Err(i),
+            None => Ok(Rna(rna.to_string()))
+        }
+    }
+}
